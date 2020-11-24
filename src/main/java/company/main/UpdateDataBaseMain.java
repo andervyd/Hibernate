8f261af82f6640017cfa40b76df52603419fb0aa
andervyd <andervyd@gmail.com>
2020-11-25 00:59:33 +0200

    added: update DB

---
+++ src/main/java/company/main/UpdateDataBaseMain.java
@@ -0,0 +1,32 @@
+package company.main;
+
+import company.entity.Employee;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class UpdateDataBaseMain {
+    public static void main(String[] args) {
+
+        try (SessionFactory factory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Employee.class).buildSessionFactory()) {
+
+            Session session = factory.getCurrentSession();
+            session.beginTransaction();
+
+/*
+            // change salary by employee = id 1
+            Employee idEmployee = session.get(Employee.class, 1);
+            idEmployee.setSalary(2800);
+*/
+
+            // change salary for name
+            session.createQuery(
+                    "UPDATE Employee SET salary = 2300 WHERE name = 'Oleg'").executeUpdate();
+
+
+            session.getTransaction().commit();
+            System.out.println("Done");
+        }
+    }
+}
